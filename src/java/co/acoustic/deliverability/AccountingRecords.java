52559147c179c3a8f1e9b1e8ba8d21cdb02a5f77
Wade Hundley <gwhundle@us.ibm.com>
2019-09-04 13:54:46 -0400

    added exception handling

---
+++ src/java/co/acoustic/deliverability/AccountingRecords.java
@@ -0,0 +1,60 @@
+/****************************************************************
+ *
+ * Copyright © 20xx Acoustic, L.P. All rights reserved.
+ *
+ * NOTICE: This file contains material that is confidential and proprietary to
+ * Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
+ * industrial property rights of Acoustic, L.P. except as may be provided in an agreement with
+ * Acoustic, L.P. Any unauthorized copying or distribution of content from this file is
+ * prohibited.
+ *
+ ****************************************************************/
+
+package co.acoustic.deliverability;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by gwhundle on 8/28/19.
+ */
+public class AccountingRecords {
+
+    Map<String, String> fieldMapping = new HashMap<String, String>() {
+        {
+            put("type", "type");
+            put("timeLogged", "t");
+            put("timeQueued", "tq");
+            put("orig", "orig");
+            put("rcpt", "r");
+            put("orcpt", "or");
+            put("dsnAction", "dsnAct");
+            put("dsnStatus", "dsnSts");
+            put("dsnDiag", "dsnDiag");
+            put("dsnMta", "dsnMTA");
+            put("srcType", "src.type");
+            put("srcMta", "src.mta");
+            put("dlvType", "dlvThrough");
+            put("dlvSourceIp", "dlvFrom");
+            put("dlvDestinationIp", "dlvTo");
+            put("dlvEsmtpAvailable", "esmtp");
+            put("dlvSize", "size");
+            put("vmta", "vmta");
+            put("jobId", "jobId");
+            put("envId", "envId");
+            put("queue", "");
+            put("vmtaPool", "");
+            put("bounceCat", "bncCat");
+            put("vmtaPool", "");
+        }
+    };
+
+    // convert CSV Field Name to Binary Field Name
+    public String convertCsvToBinary( String csvField )
+    {
+        return fieldMapping.get(csvField);
+    }
+
+
+
+}
